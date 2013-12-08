3e01db63059b660257abfb80993cf49999eeff98
Tomi Takussaari <tomi.takussaari@elisa.fi>
2013-12-08 17:36:28 +0200

    Upgrade to 0.5.0

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,6 @@
 node "puppetmaster-vagrant.local"  {
   	class { "bower_puppet_server":
-  		environments => "\"bower-puppet-master-example\": \"https://github.com/TomiTakussaari/bower-puppet-master-example.git#latest\""
+  		environments => "\"bower-puppet-master-example\": \"https://github.com/TomiTakussaari/bower-puppet-master-example.git#latest\"",
+		enable_api=>    'true'
 	}
 }
